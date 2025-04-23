db8628c092506e598f45dba8c9d6b71a8a70891b
Nitish Kumar <124133718+NewWebDeveloper1@users.noreply.github.com>
2025-04-23 15:28:44 +0530

    Add files via upload

--- api/chat.js
+++ api/chat.js
@@ -1,8 +1,18 @@
 import axios from "axios";
 
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://lingo-ai-frontend.vercel.app/",
+];
+
 export default async function handler(req, res) {
+  const origin = req.headers.origin;
+
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+  }
+
   // Allow CORS
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
   res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
